47f749762fef5a63b0b203b6cbbe87f97e312f43
7kayoh <git@7kayoh.net>
2022-05-09 02:56:58 +0900

    Added needed module requires

--- src/Router.lua
+++ src/Router.lua
@@ -1,6 +1,9 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local Fusion = require(ReplicatedStorage.Packages.Fusion)
+local Tree = require(script.Parent.Tree)
+local StateDict = require(script.Parent.StateDict)
+local Parse = require(script.Parent.Parse)
 local Types = require(script.Parent.Types)
 
 local Router = {} :: Types.Router
