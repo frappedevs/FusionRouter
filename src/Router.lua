b29831a2a192eb9afc5a9f6ce34140f37c164045
7kayoh <git@7kayoh.net>
2022-05-09 02:57:35 +0900

    Finished constructor function

--- src/Router.lua
+++ src/Router.lua
@@ -9,7 +9,28 @@ local Types = require(script.Parent.Types)
 local Router = {} :: Types.Router
 Router.__index = Router
 
-function Router.new(routes)
+function Router.new(routes: Types.Routes)
+    local routeIndices = {}
+    for path, route in pairs(routes) do
+        if type(path) == "string" then
+            route.Path = path
+        end
+        assert(type(route.Path) == "string", "Router expects route path to be a string, got " .. type(route.Path))
+        route.Path = route.Path:lower()
+        routeIndices[route.Path] = route
+    end
+    assert(routeIndices["/"], "Router expects base route \"/\" to be supplied, got nil")
+
+    local self = setmetatable({}, Router)
+    self.Routes = Tree(routeIndices["/"])
+    routeIndices["/"] = nil
+    self.CurrentPage = {}
+    for _, route in pairs(routeIndices) do
+        self:addRoute(route)
+    end
+
+    return self
+end
 
 end
 
