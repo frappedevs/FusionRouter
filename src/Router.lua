3fe7e062511418882488a0d36c40defc34eebe9d
7kayoh <git@7kayoh.net>
2022-05-09 02:59:09 +0900

    Formatted code

--- src/Router.lua
+++ src/Router.lua
@@ -10,51 +10,53 @@ local Router = {} :: Types.Router
 Router.__index = Router
 
 function Router.new(routes: Types.Routes)
-    local routeIndices = {}
-    for path, route in pairs(routes) do
-        if type(path) == "string" then
-            route.Path = path
-        end
-        assert(type(route.Path) == "string", "Router expects route path to be a string, got " .. type(route.Path))
-        route.Path = route.Path:lower()
-        routeIndices[route.Path] = route
-    end
-    assert(routeIndices["/"], "Router expects base route \"/\" to be supplied, got nil")
+	local routeIndices = {}
+	for path, route in pairs(routes) do
+		if type(path) == "string" then
+			route.Path = path
+		end
+		assert(type(route.Path) == "string", "Router expects route path to be a string, got " .. type(route.Path))
+		route.Path = route.Path:lower()
+		routeIndices[route.Path] = route
+	end
+	assert(routeIndices["/"], 'Router expects base route "/" to be supplied, got nil')
 
-    local self = setmetatable({}, Router)
-    self.Routes = Tree(routeIndices["/"])
-    routeIndices["/"] = nil
-    self.CurrentPage = {}
-    for _, route in pairs(routeIndices) do
-        self:addRoute(route)
-    end
+	local self = setmetatable({}, Router)
+	self.Routes = Tree(routeIndices["/"])
+	routeIndices["/"] = nil
+	self.CurrentPage = {}
+	for _, route in pairs(routeIndices) do
+		self:addRoute(route)
+	end
 
-    return self
+	return self
 end
 
 function Router:addRoute(route: Types.Route<string>)
-    local function resolve(path: string, node: Types.TreeChild<Types.Route<string>|{ ParameterName: string? }>)
-        local current, rest = Parse(path)
-        local isWildcard = current:match("^:")
-        if isWildcard then
-            current = "%WILDCARD%"
-        end
-        local currentNode = node[Fusion.Children][current]
+	local function resolve(path: string, node: Types.TreeChild<Types.Route<string> | { ParameterName: string? }>)
+		local current, rest = Parse(path)
+		local isWildcard = current:match("^:")
+		if isWildcard then
+			current = "%WILDCARD%"
+		end
+		local currentNode = node[Fusion.Children][current]
 
-        if currentNode and rest then -- if current route exists and theres more
-            resolve(rest, currentNode) -- resolve
-        elseif currentNode and #currentNode.Value == 0 and not rest then -- if theres no more to resolve but there is a current route with no data
-            currentNode.Value = route -- set the current route to the new route
-        elseif not currentNode then -- if theres no current route and theres more
-            currentNode:newChild({ -- create new route with empty data or route if no more
-                [current] = if not rest then route else {
-                    ParameterName = if isWildcard then current:sub(2) else nil,
-                },
-            })
-        end
-    end
+		if currentNode and rest then -- if current route exists and theres more
+			resolve(rest, currentNode) -- resolve
+		elseif currentNode and #currentNode.Value == 0 and not rest then -- if theres no more to resolve but there is a current route with no data
+			currentNode.Value = route -- set the current route to the new route
+		elseif not currentNode then -- if theres no current route and theres more
+			currentNode:newChild({ -- create new route with empty data or route if no more
+				[current] = if not rest
+					then route
+					else {
+						ParameterName = if isWildcard then current:sub(2) else nil,
+					},
+			})
+		end
+	end
 
-    resolve(route.Path, self.Routes)
+	resolve(route.Path, self.Routes)
 end
 
 function Router:setRoute(route: Types.Route<string>, parameters: { [any]: any }) end
